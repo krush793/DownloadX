a10917d4dd68b9c3fe9dcc31ac40084183713142
krush793 <72.krushna@gmail.com>
2024-08-01 14:13:01 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -15,18 +15,8 @@ inputURL.addEventListener('click', async () => {
     const text = await navigator.clipboard.readText();
     if (text.match(pattern)) {
         inputURL.value = text
-        inputURL.setCustomValidity('');
     }
-});
-inputURL.addEventListener('input', () => {
-    inputURL.setCustomValidity('');
 });
 downloadBtn.addEventListener('click', () => {
-    const pattern = /^(https?:\/\/)?(([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)*[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.[a-zA-Z]{2,})(\/[^?]*)?(\?[^#]*)?$/;
-    if (inputURL.value.trim() === '') {
-        inputURL.setCustomValidity('Please enter URL.');
-    } else if (!inputURL.value.trim().match(pattern)) {
-        inputURL.setCustomValidity('Please enter a valid URL.');
-    }
-    inputURL.reportValidity();
+    downData.postMessage('Hello');
 });
